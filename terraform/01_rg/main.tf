6f3782e1bc5aa15d3ed505e0f2742703af8b6354
Brendon Matheson <bmatheson@microsoft.com>
2025-12-18 13:41:54 +0700

    Initial working Jupyter orchestrated Terraform project

---
+++ terraform/01_rg/main.tf
@@ -0,0 +1,28 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+  }
+}
+
+variable "resource_group_name" {
+  description = "Name of the resource group"
+  type        = string
+}
+
+variable "location" {
+  description = "Azure region for the resource group"
+  type        = string
+  default     = "Sweden Central"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "rg" {
+  name     = var.resource_group_name
+  location = var.location
+}
